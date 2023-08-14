8322172702a19cc7a744cb83c3222e07e7402b94
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-08-14 17:11:12 -0300

    feat : Implementation of a UI for the API's posts.

--- MovilDosRepaso/app/src/main/java/com/example/movildosrepaso/MainScreen.kt
+++ MovilDosRepaso/app/src/main/java/com/example/movildosrepaso/MainScreen.kt
@@ -1,10 +1,61 @@
 package com.example.movildosrepaso
 
+import android.graphics.drawable.shapes.Shape
 import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.movildosrepaso.data.model.Post
 import com.example.movildosrepaso.ui.MainViewModel
 
 @Composable
 fun MainScreen(mainViewModel : MainViewModel) {
 
+    val post by mainViewModel.post.observeAsState(emptyList())
+
+    LazyColumn(Modifier.fillMaxSize().padding(vertical = 0.dp)) {
+        items(post){
+            PostView(it)
+        }
+    }
+
+}
+
+@Composable
+fun PostView(post : Post) {
+    Card(
+        modifier = Modifier.padding(8.dp),
+    ) {
+        Column(Modifier.padding(8.dp)) {
+            Text(
+                text = post.title,
+                Modifier
+                    .fillMaxWidth(),
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(
+                text = post.title,
+                Modifier
+                    .fillMaxWidth(),
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Start
+            )
+        }
+    }
 }
