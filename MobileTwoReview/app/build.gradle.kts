212afd76651107c1cd5ed2b5e9b2df14cf539767
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-08-20 12:55:14 -0300

    feat : implement room dependency

--- MobileTwoReview/app/build.gradle.kts
+++ MobileTwoReview/app/build.gradle.kts
@@ -86,6 +86,10 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
     //Test LiveData
     //testImplementation "androidx.arch.core:core-testing:2.1.0"
+
+    //Room
+    implementation ("androidx.room:room-ktx:2.5.0")
+    kapt ("androidx.room:room-compiler:2.5.0")
 }
 
 kapt {
