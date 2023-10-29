3c4bfab3260ca66d0a53f4f63a87bbfd82ace6d5
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-10-29 13:33:06 -0300

    feat: implement example with lastKnownLocation & getCurrentLocation

--- LocationExample/app/src/main/java/com/example/locationexample/MainActivity.kt
+++ LocationExample/app/src/main/java/com/example/locationexample/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.example.locationexample
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.content.ContextCompat
+import androidx.compose.ui.platform.LocalContext
 import com.example.locationexample.ui.theme.LocationExampleTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         setContent {
             LocationExampleTheme {
                 // A surface container using the 'background' color from the theme
@@ -24,8 +37,35 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
+
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    )
+                    {
+                        Button(onClick = { getLastKnownLocation() }) {
+                            Text(text = "Click me")
+                        }
+                    }
                 }
             }
         }
+    }
+    @SuppressLint("MissingPermission")
+    private fun getLastKnownLocation() {
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                // Handle location result
+                Log.i("titi", location.toString())
+            }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation() {
+        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
+            .addOnSuccessListener { location: Location? ->
+                // Handle location result
+                Log.i("titi", location.toString())
+            }
     }
 }
