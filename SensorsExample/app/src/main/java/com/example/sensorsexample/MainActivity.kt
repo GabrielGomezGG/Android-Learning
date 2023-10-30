5ee1df53d0ce9c68de042d71f842e03bfafae951
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-10-30 15:45:38 -0300

    feat: implement example with sensorManager & sensor type Linear_Acceleration

--- SensorsExample/app/src/main/java/com/example/sensorsexample/MainActivity.kt
+++ SensorsExample/app/src/main/java/com/example/sensorsexample/MainActivity.kt
@@ -1,21 +1,54 @@
 package com.example.sensorsexample
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.sensorsexample.ui.theme.SensorsExampleTheme
-import com.mutualmobile.composesensors.rememberAccelerometerSensorState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+        var isShake = false
+
+        sensorManager.registerListener(object : SensorEventListener{
+            override fun onSensorChanged(event: SensorEvent?) {
+                event.run {
+                    var x = this!!.values[0];
+                    var y = this.values[1];
+                    var z = this.values[2];
+
+                    // Calcula la aceleración total
+                    var acceleration = Math.sqrt((x * x + y * y + z * z).toDouble());
+
+                    // Comprueba si la aceleración supera el umbral de agitación
+                    if (acceleration > 40f && !isShake) {
+                        Log.i("titi", "asdasdsadas")
+                        Log.i("titi", acceleration.toString())
+                        Toast.makeText(applicationContext, "asdasdsadas", Toast.LENGTH_SHORT).show()
+                        isShake = true
+                    }
+                }
+            }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+            }
+
+        }, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+
         setContent {
             SensorsExampleTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,13 +56,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val accelerometerState = rememberAccelerometerSensorState()
-                    Text(
-                        text = "Force X: ${accelerometerState.xForce}" +
-                                "\nForce Y: ${accelerometerState.yForce}" +
-                                "\nForce Z: ${accelerometerState.zForce}" +
-                                "\nIs Available?: ${accelerometerState.isAvailable}"
-                    )
+
                 }
             }
         }
