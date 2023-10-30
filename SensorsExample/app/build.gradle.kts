198d3684ea97c536356910c0aa3b9fd6f52e0007
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-10-30 13:10:05 -0300

    feat: implement com.mutualmobile:composesensors dependency with example

--- SensorsExample/app/build.gradle.kts
+++ SensorsExample/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.sensorsexample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.sensorsexample"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -66,4 +66,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("com.mutualmobile:composesensors:1.1.2")
 }
