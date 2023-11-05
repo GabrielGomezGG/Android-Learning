086587ae6a777c85f06f62c42a416c24cc09289b
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-11-05 11:51:27 -0300

    intent not work :c

--- CamaraIntentExample/app/build.gradle.kts
+++ CamaraIntentExample/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.camaraintentexample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.camaraintentexample"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -66,4 +66,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
--- CamaraIntentExample/app/src/main/java/com/example/camaraintentexample/MainActivity.kt
+++ CamaraIntentExample/app/src/main/java/com/example/camaraintentexample/MainActivity.kt
@@ -2,17 +2,44 @@ package com.example.camaraintentexample
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import coil.compose.AsyncImage
 import com.example.camaraintentexample.ui.theme.CamaraIntentExampleTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val REQUEST_IMAGE_CAPTURE = 1
+
+    var wea: Bitmap? = null
+    var coso : Uri? = Uri.EMPTY
+
+    private val responseLauncher = registerForActivityResult(StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            //return photo
+            coso = it.data?.data
+            Log.i("titi", coso.toString())
+            wea = it.data?.extras?.get("data") as Bitmap
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,13 +49,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                    try {
-                        startActivityForResult(intent, 777)
-                    } catch (e: ActivityNotFoundException) {
-                        // Mostrar error, no hay apps que tomen fotos
-                    }
+                    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
+                    Column {
+                        Button(onClick = {
+                            responseLauncher.launch(takePictureIntent)
+                        }) {
+                            Text(text = "titi")
+                        }
+                        if(wea != null){
+                            Log.i("titi", wea.toString())
+                            Image(bitmap = wea!!.asImageBitmap(), contentDescription = null)
+                        }
+                            AsyncImage(model = coso, contentDescription = null)
+                        if(coso != emptyList<Uri>()){
+                            Log.i("titi", coso.toString())
+                        }
+                    }
                 }
             }
         }
