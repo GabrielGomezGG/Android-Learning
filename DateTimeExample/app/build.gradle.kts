ab40eaec4c412e8931a7fa80764a78ea12440550
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2024-01-18 13:39:07 -0300

    feat: date picker example

--- DateTimeExample/app/build.gradle.kts
+++ DateTimeExample/app/build.gradle.kts
@@ -32,6 +32,8 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+
+        isCoreLibraryDesugaringEnabled = true
     }
     kotlinOptions {
         jvmTarget = "1.8"
@@ -51,10 +53,12 @@ android {
 
 dependencies {
 
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:2.0.4")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -62,7 +66,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
--- DateTimeExample/app/src/main/java/com/titi/datetimeexample/MainActivity.kt
+++ DateTimeExample/app/src/main/java/com/titi/datetimeexample/MainActivity.kt
@@ -3,14 +3,32 @@ package com.titi.datetimeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.DateRangePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.titi.datetimeexample.ui.theme.DateTimeExampleTheme
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +40,74 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DateTimeExampleTheme {
-        Greeting("Android")
+    val state = rememberDatePickerState()
+
+    var showDatePicker by remember { mutableStateOf(false) }
+
+    var wea by remember {
+        mutableStateOf("")
     }
+
+    Column {
+
+        Button(onClick = {
+            showDatePicker = true
+        }) {
+            Text(text = "Show Date Picker")
+        }
+
+        Text(text = wea)
+
+        if (showDatePicker) {
+            DatePickerDialog(
+                onDismissRequest = {
+                    showDatePicker = false
+                },
+                confirmButton = {
+                    TextButton(onClick = {
+                        showDatePicker = false
+                        state.selectedDateMillis?.let {
+                            wea = Instant.ofEpochMilli(it)
+                                .atZone(ZoneId.of("UTC"))
+                                .toLocalDate().format(
+                                    DateTimeFormatter.ofPattern("dd-MM-yyyy")
+                                )
+                        }
+                    }) {
+                        Text(text = "Confirm")
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = {
+                        showDatePicker = false
+                    }) {
+                        Text(text = "Cancel")
+                    }
+                },
+            ) {
+                DatePicker(
+                    state = state,
+//                    dateValidator = { timeInMillis ->
+//                        val endCalenderDate = Calendar.getInstance()
+//                        endCalenderDate.timeInMillis = timeInMillis
+//                        endCalenderDate.set(Calendar.DATE, Calendar.DATE + 27)
+//                        timeInMillis > Calendar.getInstance().timeInMillis - 172799999 && timeInMillis < endCalenderDate.timeInMillis
+//                    },
+                )
+            }
+        }
+    }
+
+
 }
