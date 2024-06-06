d46d34ba51ab5b42c19f0d7e36cf9e686b30b1c7
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2024-06-06 13:30:42 -0300

    WorkManagerExample ejemplo finalizado

--- WorkmanagerExample/app/build.gradle.kts
+++ WorkmanagerExample/app/build.gradle.kts
@@ -15,74 +15,78 @@
  */
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.example.bluromatic"
-    compileSdk = 34
+  namespace = "com.example.bluromatic"
+  compileSdk = 34
 
-    defaultConfig {
-        applicationId = "com.example.bluromatic"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+  defaultConfig {
+    applicationId = "com.example.bluromatic"
+    minSdk = 24
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    vectorDrawables {
+      useSupportLibrary = true
     }
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = rootProject.extra["compose_compiler_version"].toString()
+  }
+  packaging {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = rootProject.extra["compose_compiler_version"].toString()
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+  }
 }
 
 dependencies {
 
-    implementation(platform("androidx.compose:compose-bom:2023.10.00"))
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation("androidx.activity:activity-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.runtime:runtime")
-    implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    debugImplementation("androidx.compose.ui:ui-tooling")
+  implementation(platform("androidx.compose:compose-bom:2023.10.00"))
+  implementation("androidx.activity:activity-compose:1.8.0")
+  implementation("androidx.activity:activity-ktx:1.8.0")
+  implementation("androidx.appcompat:appcompat:1.6.1")
+  implementation("androidx.compose.material3:material3")
+  implementation("androidx.compose.runtime:runtime")
+  implementation("androidx.compose.runtime:runtime-livedata")
+  implementation("androidx.compose.ui:ui")
+  implementation("androidx.compose.ui:ui-graphics")
+  implementation("androidx.compose.ui:ui-tooling-preview")
+  implementation("androidx.core:core-ktx:1.12.0")
+  implementation("androidx.lifecycle:lifecycle-livedata-ktx:${rootProject.extra["lifecycle_version"]}")
+  implementation("androidx.lifecycle:lifecycle-runtime-compose:${rootProject.extra["lifecycle_version"]}")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
+  implementation("androidx.work:work-runtime-ktx:2.8.1")
+  debugImplementation("androidx.compose.ui:ui-test-manifest")
+  debugImplementation("androidx.compose.ui:ui-tooling")
+
+
+  // WorkManager dependency
+  implementation("androidx.work:work-runtime-ktx:2.8.1")
 }
--- WorkmanagerExample/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ WorkmanagerExample/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -19,35 +19,71 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
+import com.example.bluromatic.getImageUri
+import com.example.bluromatic.workers.BlurWorker
+import com.example.bluromatic.workers.CleanupWorker
+import com.example.bluromatic.workers.SaveImageToFileWorker
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
-    override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
+  // New code
+  private var imageUri: Uri = context.getImageUri() // <- Add this
+  private val workManager = WorkManager.getInstance(context)
 
-    /**
-     * Create the WorkRequests to apply the blur and save the resulting image
-     * @param blurLevel The amount to blur the image
-     */
-    override fun applyBlur(blurLevel: Int) {}
+  override val outputWorkInfo: Flow<WorkInfo?> = MutableStateFlow(null)
 
-    /**
-     * Cancel any ongoing WorkRequests
-     * */
-    override fun cancelWork() {}
+  /**
+   * Create the WorkRequests to apply the blur and save the resulting image
+   * @param blurLevel The amount to blur the image
+   */
+  override fun applyBlur(blurLevel: Int) {
 
-    /**
-     * Creates the input data bundle which includes the blur level to
-     * update the amount of blur to be applied and the Uri to operate on
-     * @return Data which contains the Image Uri as a String and blur level as an Integer
-     */
-    private fun createInputDataForWorkRequest(blurLevel: Int, imageUri: Uri): Data {
-        val builder = Data.Builder()
-        builder.putString(KEY_IMAGE_URI, imageUri.toString()).putInt(KEY_BLUR_LEVEL, blurLevel)
-        return builder.build()
-    }
+    // Add WorkRequest to Cleanup temporary images
+    var continuation = workManager.beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
+
+    // Add WorkRequest to blur the image
+    val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
+
+    // New code for input data object
+    blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
+
+    // Start the work
+    //workManager.enqueue(blurBuilder.build())
+
+    // Add the blur work request to the chain
+    continuation = continuation.then(blurBuilder.build())
+
+    // Add WorkRequest to save the image to the filesystem
+    val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+      .build()
+
+    continuation = continuation.then(save)
+
+    // Start the work
+    continuation.enqueue()
+  }
+
+  /**
+   * Cancel any ongoing WorkRequests
+   * */
+  override fun cancelWork() {}
+
+  /**
+   * Creates the input data bundle which includes the blur level to
+   * update the amount of blur to be applied and the Uri to operate on
+   * @return Data which contains the Image Uri as a String and blur level as an Integer
+   */
+  private fun createInputDataForWorkRequest(blurLevel: Int, imageUri: Uri): Data {
+    val builder = Data.Builder()
+    builder.putString(KEY_IMAGE_URI, imageUri.toString()).putInt(KEY_BLUR_LEVEL, blurLevel)
+    return builder.build()
+  }
 }
