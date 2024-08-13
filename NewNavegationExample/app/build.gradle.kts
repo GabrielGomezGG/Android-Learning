aea2160673008edc7b7f5e40767425c26a136595
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2024-08-12 21:20:20 -0300

    NewNavegationExample sample example

--- NewNavegationExample/app/build.gradle.kts
+++ NewNavegationExample/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -66,4 +68,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
 }
--- NewNavegationExample/app/src/main/java/com/gg/newnavegationexample/MainActivity.kt
+++ NewNavegationExample/app/src/main/java/com/gg/newnavegationexample/MainActivity.kt
@@ -4,44 +4,118 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.gg.newnavegationexample.ui.theme.NewNavegationExampleTheme
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             NewNavegationExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = Screen.Home) {
+
+                        composable<Screen.Home> {
+                            Home(
+                                modifier = Modifier.padding(innerPadding),
+                                navigateTo = {
+                                    navController.navigate(Screen.Wea("Titititi"))
+                                }
+                            )
+                        }
+
+                        composable<Screen.Wea> {
+                            val wea = it.toRoute<Screen.Wea>()
+                            Wea(
+                                modifier = Modifier.padding(innerPadding),
+                                text = wea.id,
+                                navigateTo = {
+                                    navController.navigate(Screen.Home)
+                                }
+                            )
+                        }
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+sealed interface Screen {
+    @Serializable
+    object Home : Screen
+
+    @Serializable
+    data class Wea(val id: String) : Screen
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NewNavegationExampleTheme {
-        Greeting("Android")
+fun Home(
+    modifier: Modifier = Modifier,
+    navigateTo: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Blue),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Home",
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(
+            onClick = navigateTo,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        ) {
+            Text("Go to Wea")
+        }
+    }
+
+}
+
+@Composable
+fun Wea(
+    modifier: Modifier = Modifier,
+    text: String,
+    navigateTo: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Red),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Wea: $text",
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(
+            onClick = navigateTo,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        ) {
+            Text("Go to Home")
+        }
     }
 }
